4cbcf43f173f6b5efe2d3b200c42262e816ed385
paulina sarquis <34319671+psarks@users.noreply.github.com>
2022-08-21 23:12:10 -0700

    working on bucketlist

---
+++ src/pages/bucketlistpage/Bucketlist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSection from '../../components/HeroSection/HeroSection';
+
+const Bucketlist = () => {
+    return (
+        <>
+            
+           <HeroSection/>
+        
+        </>
+    )
+}
+
+export default Bucketlist;
