d0b3ad9837d1247ab1282e7470df88a5c4101eeb
paulina sarquis <34319671+psarks@users.noreply.github.com>
2022-08-21 19:34:53 -0700

    creating new pdf viewer

--- src/pages/cvpage/CV.js
+++ src/pages/cvpage/CV.js
@@ -1,36 +1,13 @@
-import React, { useState } from "react";
-import {Document, Page} from 'react-pdf/dist/esm/entry.webpack';
+import React from 'react';
+import Button from '../../globalStyles';
 
-function CV(){
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
-
-    function onDocumentLoadSuccess({numPages}){
-        setNumPages(numPages);
-        setPageNumber(1);
-    }
-
-    function changePage(offSet){
-        setPageNumber(prevPageNumber => prevPageNumber + offSet);
-    }
-
-    function changePageBack(){
-        changePage(-1)
-    }
-
-    function changePageNext(){
-        changePage(+1)
-    }
-    
+function CV{}{
     return(
-        <center>
-        <div className= "resume">
-            <Document file="/resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-              <Page height="800" pageNumber={pageNumber}/>  
-            </Document>
+        <div style={{textAlign:'center'}}>
+            <br/>
+            <a href="./pdf" target="_blank">
+                <Button>Resume</Button>
+            </a>
         </div>
-        </center>
     )
 }
-
-export default CV;
