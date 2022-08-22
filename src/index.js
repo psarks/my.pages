7276941c8dcff76f516c11f0542006de025fb6f9
paulina sarquis <34319671+psarks@users.noreply.github.com>
2022-08-21 22:56:29 -0700

    misc3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './pages/cvpage/cv_elements.css'
 import App from './App';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>,
   document.getElementById('root')
 );
