e63dc85aa473c5329ef82320c1a9816bee81ff60
paulina sarquis <34319671+psarks@users.noreply.github.com>
2022-08-22 10:29:43 -0700

    bucketlist video added

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,39 +1,34 @@
-import "./HeroSection";
-import { Button } from "../../globalStyles";
-
+import '../HeroSection/HeroSection_elements.css'
+import Beach from '../../videos/beach.mp4'
 import Typical from "react-typical";
 
 function HeroSection() {
   return (
-    <div className="hero-container">
-      <video src="./videos/beach.mp4" autoPlay loop muted />
-      <h1> BucketList</h1>
-      <p>May the force be with me</p>
-      
-      <video src="./videos/beach.mp4" autoPlay loop muted />
-
-      <p>
-        {" "}
-        I love to {""}
-        <Typical
-          loop={Infinity}
-          wrapper="b"
-          steps={[
-            "write a book 📖",
-            3000,
-            "go to a Champions league game⚽",
-            3000,
-            "go to a World Cup game⚽",
-            3000,
-            "travel cross country by car or by train 🚙",
-            3000,
-            "go on a safari 🦁",
-            6000,
-            "swim with sea turtles 🐢",
-          ]}
-        />
-        <h4>May the force be with me</h4>
-      </p>
+    <div className="main">
+      <video src={Beach} autoPlay loop muted/>
+      {/*<div className="hero-container">
+        <p>
+          I love to {""}
+          <Typical
+            loop={Infinity}
+            wrapper="b"
+            steps={[
+              "write a book 📖",
+              6000,
+              "go to a Champions league game⚽",
+              6000,
+              "go to a World Cup game⚽",
+              6000,
+              "travel cross country by car or by train 🚙",
+              6000,
+              "go on a safari 🦁",
+              6000,
+              "swim with sea turtles 🐢",
+            ]}
+          />
+          <h4>May the force be with me</h4>
+        </p>
+      </div>*/}
     </div>
   );
 }
