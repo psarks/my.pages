9d8f8c0490048e40cc1e0cea35fad3406a6932dc
paulina sarquis <34319671+psarks@users.noreply.github.com>
2022-08-22 13:39:38 -0700

    removed infocv import

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
-export { default as Navbar } from './Navbar/Navbar';
-export { default as InfoSection } from './InfoSection/infoSection';
-export {default  as InfoPhoto } from './InfoPhoto/infoPhoto'
-export { default as InfoCV } from './InfoCV/infoCV'
-export {default as Footer } from './Footer/Footer';
-export {default as ImageSlider } from './ImageSlider/ImageSlider'
-export {default as HeroSection} from './HeroSection/HeroSection'
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as InfoSection } from "./InfoSection/infoSection";
+export { default as InfoPhoto } from "./InfoPhoto/infoPhoto";
+export { default as Footer } from "./Footer/Footer";
+export { default as ImageSlider } from "./ImageSlider/ImageSlider";
+export { default as HeroSection } from "./HeroSection/HeroSection";
